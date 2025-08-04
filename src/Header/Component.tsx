34e46db9277f6b0f8fe6b11c7f25878e421d0652
Prijun Koirala <191650188+koiralaprijun00@users.noreply.github.com>
2025-08-04 18:31:43 +0300

    push

--- src/Header/Component.tsx
+++ src/Header/Component.tsx
@@ -1,9 +1,17 @@
 import { HeaderClient } from './Component.client'
 import React from 'react'
 import { getCachedGlobal } from '@/utilities/getGlobals'
-import type { Header as HeaderType } from '@/payload-types'
+import type { Header as HeaderType, Media } from '@/payload-types'
 
 export async function Header() {
-  const headerData = (await getCachedGlobal('header', 1)()) as HeaderType
-  return <HeaderClient navItems={headerData.navItems || []} logo={headerData.logo || null} />
+  const headerData = (await getCachedGlobal('header', 2)()) as HeaderType
+
+  // Handle the case where logo might be a string ID or populated Media object
+  let logoData: Media | null = null
+  if (headerData.logo && typeof headerData.logo !== 'string') {
+    // If it's a populated Media object
+    logoData = headerData.logo as Media
+  }
+
+  return <HeaderClient navItems={headerData.navItems || []} logo={logoData} />
 }
